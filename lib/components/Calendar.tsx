446e0c3847a349d0869cef80b7a169d1f8660f81
FacundoBraida98 <facundobraida98@gmail.com>
2023-12-13 16:24:43 -0300

    features

--- lib/components/Calendar.tsx
+++ lib/components/Calendar.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
-} from "@heroicons/react/24/solid/index.js";
-import { formatMonth } from "../utils/date_formatter";
+} from '@heroicons/react/24/solid/index.js';
+import { formatMonth } from '../utils/date_formatter';
 import {
   add,
   eachDayOfInterval,
@@ -13,22 +13,23 @@ import {
   isToday,
   parse,
   startOfToday,
-} from "date-fns";
-import { useState } from "react";
-import { isBefore } from "date-fns/fp";
+  isBefore,
+} from 'date-fns';
+import { useState } from 'react';
+import { bg } from 'date-fns/locale';
 
 function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
+  return classes.filter(Boolean).join(' ');
 } /*  */
 
-function isPass(day: any) {
+function isFuture(day: any) {
   return isBefore(startOfToday(), day);
 }
 
 export default function Calendar(props: any) {
   const today = startOfToday();
-  const [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
-  const firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date());
+  const [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'));
+  const firstDayCurrentMonth = parse(currentMonth, 'MMM-yyyy', new Date());
 
   const days = eachDayOfInterval({
     start: firstDayCurrentMonth,
@@ -36,11 +37,11 @@ export default function Calendar(props: any) {
   });
   function previousMonth() {
     const firstDayNextMonth = add(firstDayCurrentMonth, { months: -1 });
-    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
   }
   function nextMonth() {
     const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
-    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+    setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
   }
 
   return (
@@ -48,7 +49,7 @@ export default function Calendar(props: any) {
       <div className="flex flex-col md:mx-auto md:w-1/2  text-center px-2 my-2">
         <h3 className="text-2xl px-2 text-white">Seleccione la fecha</h3>
       </div>
-      <div className="mx-auto w-full bg-red-100">
+      <div className="mx-auto lg:w-4/5 bg-black">
         <div className="flex items-center text-center">
           <button
             type="button"
@@ -57,7 +58,7 @@ export default function Calendar(props: any) {
           >
             <ChevronLeftIcon className="w-8 h-8" aria-hidden="true" />
           </button>
-          <h2 className="flex-auto font-semibold text-2xl text-gray-900 ">
+          <h2 className="flex-auto font-semibold text-2xl text-white ">
             {formatMonth(firstDayCurrentMonth)}
           </h2>
 
@@ -70,41 +71,39 @@ export default function Calendar(props: any) {
           </button>
         </div>
         <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-red-400 ">
-          <div className="w-20 ">D</div>
-          <div className="w-20 ">L</div>
-          <div className="w-20 ">M</div>
-          <div className="w-20 ">M</div>
-          <div className="w-20 ">J</div>
-          <div className="w-20 ">V</div>
-          <div className="w-20 ">S</div>
+          <div className="w-full ">D</div>
+          <div className="w-full ">L</div>
+          <div className="w-full ">M</div>
+          <div className="w-full ">M</div>
+          <div className="w-full ">J</div>
+          <div className="w-full ">V</div>
+          <div className="w-full ">S</div>
         </div>
-        <div className="grid grid-cols-7 mt-2 text-sm ">
+        <div className="grid grid-cols-7 mt-2 text-sm gap-2 p-2">
           {days.map((day, dayIdx) => (
             <div
               key={day.toString()}
               className={classNames(
                 dayIdx === 0 && colStartClasses[getDay(day)],
-                "h-28 w-20 border-solid border-2 border-red-200"
+                'h-28 w-full border-solid border-2 border-white'
               )}
             >
               <button
                 type="button"
                 className={classNames(
-                  !isPass(day) &&
-                    "disabled:text-red-400 disabled:cursor-not-allowed",
-                  "text-gray-900",
-                  isToday(day) && "text-white",
-                  !isToday(day) && "text-gray-900",
-                  isToday(day) && "bg-primary",
-                  !isToday(day) && "bg-booking-card",
-                  "enabled:hover:bg-gray-200",
-                  isToday(day) && "font-semibold",
-                  "mx-auto flex h-8 w-8 items-center justify-center rounded-full",
-                  isPass(day) && "disabled:text-gray-400"
+                  'text-white',
+                  isToday(day) && 'text-white',
+                  !isToday(day) && 'text-gray-900',
+                  isToday(day) && 'bg-primary',
+                  !isToday(day) && 'bg-booking-card',
+                  isToday(day) && 'font-semibold',
+                  isFuture(day) && 'bg-gray-400',
+                  isToday(day) && 'bg-red-300',
+                  'w-full h-full flex justify-start p-2'
                 )}
               >
-                <time dateTime={format(day, "yyyy-MM-dd")}>
-                  {format(day, "d")}
+                <time dateTime={format(day, 'yyyy-MM-dd')}>
+                  {format(day, 'd')}
                 </time>
               </button>
             </div>
@@ -116,11 +115,11 @@ export default function Calendar(props: any) {
 }
 
 let colStartClasses = [
-  "",
-  "col-start-2",
-  "col-start-3",
-  "col-start-4",
-  "col-start-5",
-  "col-start-6",
-  "col-start-7",
+  '',
+  'col-start-2',
+  'col-start-3',
+  'col-start-4',
+  'col-start-5',
+  'col-start-6',
+  'col-start-7',
 ];
