fc1d5382cf94cdfeb082dabd31c0709847a93be8
BraidaFac <facundobraida98@gmail.com>
2024-08-08 13:55:31 -0300

    se agrego ssl

--- lib/utils/constanst.ts
+++ lib/utils/constanst.ts
@@ -1,2 +1,2 @@
 //export const API_BACKEND = 'https://booking-suit-back-nest.onrender.com';
-export const API_BACKEND = 'http://vps-4250549-x.dattaweb.com:3000';
+export const API_BACKEND = 'https://vps-4250549-x.dattaweb.com';
