b47ae3bd58cec86838f0ad0dae3cf96dd8f9461b
BraidaFac <facundobraida98@gmail.com>
2024-11-25 15:41:13 -0300

    backend endpoint changed

--- lib/utils/constanst.ts
+++ lib/utils/constanst.ts
@@ -1,3 +1,3 @@
 //export const API_BACKEND = 'https://booking-suit-back-nest.onrender.com';
-//export const API_BACKEND = "https://vps-4250549-x.dattaweb.com";
-export const API_BACKEND = "http://localhost:3001";
+export const API_BACKEND = "https://vps-4250549-x.dattaweb.com";
+//export const API_BACKEND = "http://localhost:3001";
