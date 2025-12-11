3fc0e6fed9e6b976b4d0d901711e4c6c43ba8451
BraidaFac <facundobraida98@gmail.com>
2025-12-10 21:46:02 -0300

    soon returns

--- app/planillas/devolucion/page.tsx
+++ app/planillas/devolucion/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { Booking, BookingState } from "@/lib/utils/Booking";
+import { API_BACKEND } from "@/lib/utils/constanst";
 import { SuitState } from "@/lib/utils/Suit";
 import { useUserState } from "@/lib/utils/UserState";
-import { getCookie, setCookie } from "cookies-next";
 import {
+  Button,
+  Input,
+  Spinner,
   Table,
   TableBody,
   TableCell,
@@ -11,14 +14,11 @@ import {
   TableHeader,
   TableRow,
   getKeyValue,
-  Button,
-  Spinner,
-  Input,
 } from "@nextui-org/react";
+import { getCookie } from "cookies-next";
 import { format } from "date-fns";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { API_BACKEND } from "@/lib/utils/constanst";
 import toast from "react-hot-toast";
 
 export default function Devolucion() {
@@ -29,6 +29,7 @@ export default function Devolucion() {
         suit_id: string;
         client_name: string;
         client_phone: string;
+        next_booking_date: string;
         booking_date: string;
         actions: JSX.Element;
       }[]
@@ -42,6 +43,7 @@ export default function Devolucion() {
       suit_id: string;
       client_name: string;
       client_phone: string;
+      next_booking_date: string;
       booking_date: string;
       actions: JSX.Element;
     }[]
@@ -49,62 +51,108 @@ export default function Devolucion() {
 
   const fetchBookingsToReturn = async () => {
     const res = await fetch(`${API_BACKEND}/booking`);
-    const bookings = await res.json();
+    const bookings: Booking[] = await res.json();
 
-    const bookingstoReturn = bookings.filter(
+    const bookingsToReturn: Booking[] = bookings.filter(
       (booking: Booking) => booking.booking_state === BookingState.INPROGRESS
     );
-    bookingsToReturn.sort((a, b) => {
-      if (a.booking_date < b.booking_date) {
+
+    const bookingToReturnWithSoonBookingDate = bookingsToReturn.map(
+      (booking: Booking) => {
+        const suitBookings: Booking[] = bookings.filter(
+          (suitBooking: Booking) => suitBooking.suit.id === booking.suit.id
+        );
+        const soon_booking_date_string = suitBookings.reduce(
+          (acc: Booking | undefined, booking) => {
+            const date_booking = new Date(booking.booking_date).getTime();
+            const acc_date = acc
+              ? new Date(acc.booking_date).getTime()
+              : Infinity;
+
+            if (
+              date_booking > new Date().getTime() &&
+              date_booking < acc_date &&
+              booking.booking_state === BookingState.ACTIVED
+            ) {
+              return booking;
+            } else {
+              return acc;
+            }
+          },
+          undefined
+        )?.booking_date;
+
+        return {
+          ...booking,
+          soon_booking_date: soon_booking_date_string
+            ? new Date(soon_booking_date_string)
+            : undefined,
+        };
+      }
+    );
+
+    bookingToReturnWithSoonBookingDate.sort((a, b) => {
+      if (
+        a.soon_booking_date &&
+        a.soon_booking_date < (b.soon_booking_date || Infinity)
+      ) {
         return -1;
       }
-      if (a.booking_date > b.booking_date) {
+      if (
+        a.soon_booking_date &&
+        a.soon_booking_date > (b.soon_booking_date || -Infinity)
+      ) {
         return 1;
       }
       return 0;
     });
 
     setBookingsToReturn(
-      bookingstoReturn.map((booking: Booking) => {
-        return {
-          key: booking.id,
-          suit_id: booking.suit.id,
-          client_name: booking.client_name,
-          client_phone: booking.client_phone,
-          booking_date: format(new Date(booking.booking_date), "dd-MM-yyyy"),
-          actions: (
-            <Button
-              size="sm"
-              className="w-8"
-              color="primary"
-              onClick={async () => {
-                const res = await fetch(
-                  `${API_BACKEND}/booking/${booking.id}/estados`,
-                  {
-                    method: "PATCH",
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                      booking_state: BookingState.COMPLETED,
-                      suit_state: SuitState.ENLOCALSUCIO,
-                      booking_return_suit: new Date(),
-                    }),
+      bookingToReturnWithSoonBookingDate.map(
+        (booking: Booking & { soon_booking_date: Date | undefined }) => {
+          return {
+            key: booking.id,
+            suit_id: booking.suit.id,
+            client_name: booking.client_name,
+            client_phone: booking.client_phone,
+            booking_date: format(new Date(booking.booking_date), "dd-MM-yyyy"),
+            next_booking_date: booking.soon_booking_date
+              ? format(new Date(booking.soon_booking_date), "dd-MM-yyyy")
+              : "No tiene",
+            actions: (
+              <Button
+                size="sm"
+                className="w-8"
+                color="primary"
+                onClick={async () => {
+                  const res = await fetch(
+                    `${API_BACKEND}/booking/${booking.id}/estados`,
+                    {
+                      method: "PATCH",
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({
+                        booking_state: BookingState.COMPLETED,
+                        suit_state: SuitState.ENLOCALSUCIO,
+                        booking_return_suit: new Date(),
+                      }),
+                    }
+                  );
+                  if (res.ok) {
+                    toast.success("Traje devuelto correctamente");
+                    await fetchBookingsToReturn();
+                  } else {
+                    toast.error("Error al devolver traje");
                   }
-                );
-                if (res.ok) {
-                  toast.success("Traje devuelto correctamente");
-                  await fetchBookingsToReturn();
-                } else {
-                  toast.error("Error al devolver traje");
-                }
-              }}
-            >
-              Devuelto
-            </Button>
-          ),
-        };
-      })
+                }}
+              >
+                Devuelto
+              </Button>
+            ),
+          };
+        }
+      )
     );
   };
 
@@ -120,6 +168,7 @@ export default function Devolucion() {
         client_name: string;
         client_phone: string;
         booking_date: string;
+        next_booking_date: string;
         actions: JSX.Element;
       }) =>
         booking.suit_id.toLowerCase().includes(searchTermLower) ||
@@ -180,6 +229,7 @@ export default function Devolucion() {
     { key: "suit_id", label: "Traje" },
     { key: "client_name", label: "Nombre" },
     { key: "client_phone", label: "Telefono" },
+    { key: "next_booking_date", label: "Proxima reserva" },
     { key: "booking_date", label: "Fecha Reserva" },
     { key: "actions", label: "Acciones" },
   ];
